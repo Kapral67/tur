1d1f30de50defef86714b725ed05186f9849f499
Maxwell Kapral <git@maxkapral.com>
2024-02-19 16:51:58 -0800

    first compile on tur

--- tur-on-device/awscli/build.sh
+++ tur-on-device/awscli/build.sh
@@ -5,24 +5,23 @@
 TERMUX_PKG_HOMEPAGE=https://aws.amazon.com/cli
 TERMUX_PKG_DESCRIPTION="A Unified Tool to Manage Your AWS Services"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION="2.15.21"
 TERMUX_PKG_SRCURL="https://awscli.amazonaws.com/awscli-${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256="SKIP_CHECKSUM" # verified using gpg signatures instead
-TERMUX_PKG_BUILD_DEPENDS="python, python-pip"
+# TERMUX_PKG_BUILD_DEPENDS="gpgme, python-pip"
 TERMUX_PKG_SETUP_PYTHON=true
 # TERMUX_PKG_PYTHON_COMMON_DEPS="setuptools>=62.4, setuptools_rust, cffi, wheel"
+TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --prefix=$TERMUX_PREFIX
 --with-install-type=portable-exe
 --with-download-deps
 "
-
 TERMUX_PKG_AUTO_UPDATE=true
 
 _import_awscli_pgp_key() {
-
     # This key expired 2023-09-17 but it is still in use
     # Found at https://docs.aws.amazon.com/cli/latest/userguide/getting-started-source-install.html#source-getting-started-install-reqs
     cat <<EOF | gpg --import -
@@ -56,20 +55,24 @@ YLZATHZKTJyiqA==
 =vYOk
 -----END PGP PUBLIC KEY BLOCK-----
 EOF
-
 }
 
-termux_pkg_auto_update() {
+_get_awscli_src_tarball() {
     local tarball
     local sig
-    local version
 
     _import_awscli_pgp_key
 
     tarball="$(mktemp -p "$TERMUX_PKG_TMPDIR" "awscli.XXXXXX.tar.gz")"
     sig="$(mktemp -p "$TERMUX_PKG_TMPDIR" "awscli.XXXXXX.sig")"
-    wget -O "$tarball" https://awscli.amazonaws.com/awscli.tar.gz
-    wget -O "$sig" https://awscli.amazonaws.com/awscli.tar.gz.sig
+
+    if [ "$1" = "--latest" ]; then
+        curl -Lo "$tarball" https://awscli.amazonaws.com/awscli.tar.gz
+        curl -Lo "$sig" https://awscli.amazonaws.com/awscli.tar.gz.sig
+    else
+        curl -Lo "$tarball" "$TERMUX_PKG_SRCURL"
+        curl -Lo "$sig" "${TERMUX_PKG_SRCURL}.sig"
+    fi
 
     if ! gpg --verify "$sig" "$tarball"; then
         rm -f "$sig"
@@ -79,8 +82,15 @@ termux_pkg_auto_update() {
 
     rm -f "$sig"
 
-    version="$(tar tzf "$tarball" | head -n 1 | grep -oP "\d+\.\d+\.\d+")"
+    echo "$tarball"
+}
 
+termux_pkg_auto_update() {
+    local tarball
+    local version
+
+    tarball="$(_get_awscli_src_tarball --latest)"
+    version="$(tar tzf "$tarball" | head -n 1 | grep -oP "\d+\.\d+\.\d+")"
     rm -f "$tarball"
 
     if [ -z "$version" ]; then
@@ -94,40 +104,25 @@ termux_pkg_auto_update() {
 
 termux_step_get_source() {
     local tarball
-    local sig
-
-    _import_awscli_pgp_key
-
-    tarball="$(mktemp -p "$TERMUX_PKG_TMPDIR" "awscli.XXXXXX.tar.gz")"
-    sig="$(mktemp -p "$TERMUX_PKG_TMPDIR" "awscli.XXXXXX.sig")"
-
-    wget -O "$tarball" "$TERMUX_PKG_SRCURL"
-    wget -O "$sig" "$TERMUX_PKG_SRCURL.sig"
-
-    if ! gpg --verify "$sig" "$tarball"; then
-        rm -f "$sig"
-        rm -f "$tarball"
-        termux_error_exit "Error: failed to validate upstream source code"
-    fi
-
-    rm -f "$sig"
-
+    tarball="$(_get_awscli_src_tarball)"
     mkdir -p "$TERMUX_PKG_SRCDIR"
-
     tar --strip-components=1 -xf "$tarball" -C "$TERMUX_PKG_SRCDIR"
-
     rm -f "$tarball"
 }
 
 termux_step_pre_configure() {
+    if [ "${TERMUX_ON_DEVICE_BUILD}" = false ]; then
+        termux_error_exit "This package doesn't support cross-compiling."
+    fi
+
     # CFLAGS+=" -Wno-incompatible-function-pointer-types"
     # termux_setup_cmake
     # termux_setup_rust
     # termux_setup_python_pip
 
-    echo -e "\n$(python --version)\n"
-    python -m venv "$TERMUX_PKG_TMPDIR/awscli-venv"
-    echo -e "\n$(pip --version)\n"
+    # echo -e "\n$(python --version)\n"
+    # python -m venv "$TERMUX_PKG_TMPDIR/awscli-venv"
+    # echo -e "\n$(pip --version)\n"
 
     # pip3 install \
     #     'flit_core>=3.7.1,<3.9.1' \
@@ -145,5 +140,5 @@ termux_step_pre_configure() {
 
     # pip install "$TERMUX_PKG_SRCDIR"
 
-    exit 1
+    # exit 1
 }
